6183ab72da0bd19939af64e022ddc6edc37c78d0
Evan Jones <evanjones@fastmail.net>
2020-01-03 12:03:53 -0500

    added comments

--- models/bike.js
+++ models/bike.js
@@ -1,14 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 
+// schema for recording bike info
 const BikeSchema = new Schema({
   distance: String,
   minutes: Number,
   seconds: Number,
-  created: { type: Date, default: Date.now },
+  created: { type: Date, default: Date.now }
 });
 
-const Bike = mongoose.model('Bike', BikeSchema);
+const Bike = mongoose.model("Bike", BikeSchema);
 
 module.exports = Bike;
